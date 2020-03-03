fd661a031001711c8b90c769273404c70a36fb5a
better_git <576146819@qq.com>
2020-03-03 12:30:38 +0800

    <update> 增加了错误输入的提示，防止输出无效结果。

--- AverageGPA/AverageGPA.c
+++ AverageGPA/AverageGPA.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#define MAX_SIZE 100
-int Input(float (*)[2],int *);
-int Calculate(float (*)[2], int,float *,float *,float *);
-int Output(float,float,float,int);
+#include <string.h>
 
-int main(){
+#define MAX_SIZE 100
+int Input(float (*)[2], int *);
+int Calculate(float (*)[2], int, float *, float *, float *);
+int Output(float, float, float, int);
+
+int main() {
     float GPA[100][2];
     int success;
     int num_class;
     float total_Credit;
 
-    success =Input(GPA,&num_class);//input the GPA into the matrix "GPA".
-    if(success!=0){
+    success = Input(GPA, &num_class); // input the GPA into the matrix "GPA".
+    if (success != 0) {
         printf("Something wrong with function 'Input'!\n");
         return -1;
     }
 
-    float averScore,averGPA;
-    success=Calculate(GPA,num_class,&averScore,&averGPA,&total_Credit);
-    if(success!=0){
+    float averScore, averGPA;
+    success = Calculate(GPA, num_class, &averScore, &averGPA, &total_Credit);
+    if (success != 0) {
         printf("Something wrong with function 'Calculate'!\n");
         return -1;
     }
 
-    success =Output(averScore,averGPA,total_Credit,num_class);
-    if(success!=0){
+    success = Output(averScore, averGPA, total_Credit, num_class);
+    if (success != 0) {
         printf("Something wrong with function 'Output'!\n");
         return -1;
     }
@@ -35,60 +36,68 @@ int main(){
     return 0;
 }
 
-int Input(float (*GPA_point)[2], int *num_class){
-    memset(GPA_point,0,sizeof(float)*200);
-    printf("Waiting for reading files from GPA.txt...\n");
+int Input(float (*GPA_point)[2], int *num_class) {
+    memset(GPA_point, 0, sizeof(float) * 200);
+    printf("Waiting for reading files from GPA.csv...\n");
     FILE *fp;
-    fp=fopen("GPA.csv","r");
-    int i=0;
+    if ((fp = fopen("GPA.csv", "r")) == NULL) {
+        printf("The GPA.csv file does not exist! Please put \
+        GPA.csv under the same path of the execution file.");
+    }
+    int i = 0;
     char buffer[MAX_SIZE];
-    fgets(buffer,MAX_SIZE,fp); // 读入的时候跳过第一行。
-    while(fgets(buffer,MAX_SIZE,fp)!=NULL){
-        char *result=NULL;
-        result=strtok(buffer,",");
-        int j=0;
-        while(result!=NULL){
-            if(j==4){//学分
-               GPA_point[i][0]=atof(result);
+    fgets(buffer, MAX_SIZE, fp); // 读入的时候跳过第一行。
+    while (fgets(buffer, MAX_SIZE, fp) != NULL) {
+        char *result = NULL;
+        result = strtok(buffer, ",");
+        int j = 0;
+        while (result != NULL) {
+            if (j == 4) { //学分
+                GPA_point[i][0] = atof(result);
+            } else if (j == 6) { //分数
+                GPA_point[i][1] = atof(result);
             }
-            else if(j==6){//分数
-                GPA_point[i][1]=atof(result);
-            }
-            result=strtok(NULL,",");
+            result = strtok(NULL, ",");
             j++;
+            if (result == NULL && j != 7) {
+                printf("Please check your csv file! There some blanks have "
+                       "not been filled yet.\n");
+                return -1;
+            }
         }
-        printf("credit is %f, score is:%f\n",GPA_point[i][0],GPA_point[i][1]);
+        printf("credit is %f, score is:%f\n", GPA_point[i][0], GPA_point[i][1]);
         i++;
     }
     fclose(fp);
     printf("Done\n");
-    *num_class=i;
+    *num_class = i;
     return 0;
 }
 
-int Calculate(float (*GPA_point)[2],int num_class,float *averScore,float *averGPA,float *total_Credit){
-    *total_Credit=0;
-    for(int i=0;i<num_class;i++){
+int Calculate(float (*GPA_point)[2], int num_class, float *averScore,
+              float *averGPA, float *total_Credit) {
+    *total_Credit = 0;
+    for (int i = 0; i < num_class; i++) {
         *total_Credit += GPA_point[i][0];
     }
 
-    for(int i=0;i<num_class;i++){
-        *averScore+=GPA_point[i][0]/(*total_Credit)*GPA_point[i][1];
+    for (int i = 0; i < num_class; i++) {
+        *averScore += GPA_point[i][0] / (*total_Credit) * GPA_point[i][1];
         float GPA;
-        if(GPA_point[i][1]>=85){
-            GPA=4.0f;
-        }else{
-            GPA=4.0-(85-GPA_point[i][1])/10;
+        if (GPA_point[i][1] >= 85) {
+            GPA = 4.0f;
+        } else {
+            GPA = 4.0 - (85 - GPA_point[i][1]) / 10;
         }
-        *averGPA+=GPA_point[i][0]/(*total_Credit)*GPA;
+        *averGPA += GPA_point[i][0] / (*total_Credit) * GPA;
     }
     return 0;
 }
 
-int Output(float averScore, float averGPA,float total_Credit,int num_class){
+int Output(float averScore, float averGPA, float total_Credit, int num_class) {
     printf("Your number of classes is: %d\n", num_class);
-    printf("Your total credit is: %f\n",total_Credit);
-    printf("Your average Score is: %f\n",averScore);
-    printf("Your average GPA is: %f\n",averGPA);
+    printf("Your total credit is: %f\n", total_Credit);
+    printf("Your average Score is: %f\n", averScore);
+    printf("Your average GPA is: %f\n", averGPA);
     return 0;
 }
